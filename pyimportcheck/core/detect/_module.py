75c944678a0262240597b5afd3fad9e25060f4dc
Yann MAGNIN <yann.magnin@protonmail.com>
2024-08-04 10:54:40 +0200

    PyImportCheck - v0.0.4-dev10 : add missing broken module tests
    *add*
    > [tests]
      | [data] add a fake broken module
      | [detect] add broken module detection tests
    
    *fix*
    > [core]
      | [detect] fix notification information in broken module detection
      | [detect] fix broken root module detection

--- pyimportcheck/core/detect/_module.py
+++ pyimportcheck/core/detect/_module.py
@@ -10,6 +10,40 @@ from pyimportcheck.core.scan import PicScannedModule
 from pyimportcheck.core.detect.types import PicDetectNotification
 
 #---
+# Internals
+#---
+
+def _pic_generate_notification(
+    root:   PicScannedModule,
+    module: PicScannedModule,
+) -> PicDetectNotification:
+    """ generate a notification
+    """
+    pathfile = module.path.resolve().relative_to(
+        (root.path/'..').resolve(),
+    )
+    return PicDetectNotification(
+        type    = 'error',
+        path    = pathfile,
+        log     = f"{pathfile}: missing critical `__init__.py` file",
+    )
+
+
+def _pic_detect_module_invalid(
+    root:    PicScannedModule,
+    current: PicScannedModule,
+) -> List[PicDetectNotification]:
+    """ check missing `__init__.py` file
+    """
+    notifications: List[PicDetectNotification] = []
+    if '__init__' not in current.modules:
+        notifications.append(_pic_generate_notification(root, current))
+    for module_info in current.modules.values():
+        if isinstance(module_info, PicScannedModule):
+            notifications += _pic_detect_module_invalid(root, module_info)
+    return notifications
+
+#---
 # Public
 #---
 
@@ -18,19 +52,4 @@ def pic_detect_module_invalid(
 ) -> List[PicDetectNotification]:
     """ check missing `__init__.py` file
     """
-    notifications: List[PicDetectNotification] = []
-    for _, module_info in current.modules.items():
-        if not isinstance(module_info, PicScannedModule):
-            continue
-        if '__init__' not in module_info.modules:
-            notifications.append(
-                PicDetectNotification(
-                    type    = 'error',
-                    path    = module_info.path,
-                    log     = \
-                        f"{module_info.path}: missing critical "
-                        '`__init__.py` file'
-                )
-            )
-        notifications += pic_detect_module_invalid(module_info)
-    return notifications
+    return _pic_detect_module_invalid(current, current)
---
+++ tests/test_detect/test_module.py
@@ -0,0 +1,47 @@
+"""
+tests.test_detec.test_module    - check broken module
+"""
+from pathlib import Path
+
+from pyimportcheck.core.scan import pic_scan_package
+from pyimportcheck.core.detect._module import pic_detect_module_invalid
+from pyimportcheck.core.detect import PicDetectNotification
+
+#---
+# Internals
+#---
+
+_PREFIX_PKG = Path(f"{__file__}/../../_data/brokenpkg").resolve()
+_SCANINFO   = [
+    PicDetectNotification(
+        type    = 'error',
+        path    = Path('brokenpkg'),
+        log     = 'brokenpkg: missing critical `__init__.py` file',
+    ),
+    PicDetectNotification(
+        type        = 'error',
+        path        = Path('brokenpkg/prout'),
+        log         = \
+            'brokenpkg/prout: missing critical `__init__.py` file',
+    ),
+]
+
+#---
+# Public
+#---
+
+def test_module() -> None:
+    """ test broken module detection
+    """
+    scaninfo = pic_scan_package(_PREFIX_PKG)
+    detectinfo = pic_detect_module_invalid(scaninfo)
+    print('-== check each notification ==-')
+    for notif in detectinfo:
+        print(notif.debug_show())
+    print('-== check notification validity ==-')
+    for notif in _SCANINFO:
+        print(f"looking for ==> {notif.debug_show()}")
+        assert notif in detectinfo
+        detectinfo.remove(notif)
+    print("-== check remaining notifications ==-")
+    assert len(detectinfo) == 0
